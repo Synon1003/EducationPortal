9a3d60de1c17b967f88de0cce3224be494de26ba
Zdibák Zoltán <zdibakzoltan@gmail.com>
2025-08-22 19:01:08 +0200

    refact(Views) Linking page structure

--- EducationPortal.Web/Controllers/HomeController.cs
+++ EducationPortal.Web/Controllers/HomeController.cs
@@ -6,26 +6,16 @@ namespace EducationPortal.Web.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+    public HomeController()
+    { }
 
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        HomeViewModel model = new HomeViewModel
+        {
+            Header = "Welcome to the Education Portal",
+            WelcomeText = "Your gateway to knowledge and learning."
+        };
+        return View(model);
     }
 }
---
+++ EducationPortal.Web/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace EducationPortal.Web.Models;
+
+public class HomeViewModel
+{
+    public string Header { get; set; }
+
+    public string WelcomeText { get; set; }
+}
