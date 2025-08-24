6e5a843812172a10a5949146402eec8ef045d5a1
Zdibák Zoltán <zdibakzoltan@gmail.com>
2025-08-24 15:07:41 +0200

    feat(EntityFrameworkRepository): Created AddRangeAsync and changed Exists to check by filter

--- EducationPortal.Data/Repositories/Interfaces/IRepository.cs
+++ EducationPortal.Data/Repositories/Interfaces/IRepository.cs
@@ -4,7 +4,8 @@ public interface IRepository<TEntity> where TEntity : class
 {
     Task<ICollection<TEntity>> GetAllAsync();
     Task<TEntity?> GetByIdAsync(int id);
-    Task<bool> Exists(int id);
+    bool Exists(Func<TEntity, bool> predicate);
     Task InsertAsync(TEntity entity);
+    Task InsertRangeAsync(List<TEntity> entities);
     Task UpdateAsync(TEntity entity);
 }
