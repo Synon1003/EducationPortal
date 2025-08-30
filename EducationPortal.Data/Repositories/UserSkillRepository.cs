9d001875497132019e344f8a9db45ed70901e206
Zdibák Zoltán <zdibakzoltan@gmail.com>
2025-08-30 14:56:28 +0200

    refact(EntityFrameworkJoinRepository): Created base repository for join tables

--- EducationPortal.Data/Repositories/UserSkillRepository.cs
+++ EducationPortal.Data/Repositories/UserSkillRepository.cs
@@ -1,36 +1,10 @@
 using EducationPortal.Data.Repositories.Interfaces;
 using EducationPortal.Data.Entities;
-using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
 
 namespace EducationPortal.Data.Repositories;
 
-public class UserSkillRepository : IUserSkillRepository
+public class UserSkillRepository : EntityFrameworkJoinRepository<UserSkill>, IUserSkillRepository
 {
-    protected EducationPortalDbContext _context;
-
-    public UserSkillRepository(EducationPortalDbContext context)
-    {
-        _context = context;
-    }
-
-    public bool Exists(Func<UserSkill, bool> predicate)
-    {
-        return _context.Set<UserSkill>().AsNoTracking().Any(predicate);
-    }
-
-    public async Task<UserSkill?> GetByFilterAsync(Expression<Func<UserSkill, bool>> predicate)
-    {
-        return await _context.Set<UserSkill>().AsNoTracking().Where(predicate).FirstOrDefaultAsync();
-    }
-
-    public async Task InsertAsync(UserSkill userSkill)
-    {
-        await _context.Set<UserSkill>().AddAsync(userSkill);
-    }
-
-    public void Update(UserSkill userSkill)
-    {
-        _context.Set<UserSkill>().Update(userSkill);
-    }
+    public UserSkillRepository(EducationPortalDbContext context) : base(context)
+    { }
 }
